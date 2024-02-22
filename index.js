92926fc05a4bdb1317b15ad829652b0625a84ae4
Tavo López Hernández <61797363+ovatlh@users.noreply.github.com>
2024-02-21 18:45:13 -0600

    update portfolio | Part 2
    * temas

--- index.js
+++ index.js
@@ -1,6 +1,42 @@
 const tavo_desktop_DOM = document.getElementById("tavo-desktop");
 const tavo_desktop_title_DOM = document.getElementById("tavo-desktop-title");
 
+const  btn_light_DOM = document.getElementById("btn-light");
+const  btn_dark_DOM = document.getElementById("btn-dark");
+
+function fn_theme_light() {
+  btn_dark_DOM.classList.remove("active");
+  btn_light_DOM.classList.add("active");
+
+  document.body.classList.remove("dark");
+
+  localStorage.setItem("theme", "light");
+}
+
+function fn_theme_dark() {
+  btn_light_DOM.classList.remove("active");
+  btn_dark_DOM.classList.add("active");
+
+  document.body.classList.add("dark");
+
+  localStorage.setItem("theme", "dark");
+}
+
+function fn_theme_init() {
+  const theme = localStorage.getItem("theme");
+
+  btn_dark_DOM.classList.remove("active");
+  btn_light_DOM.classList.remove("active");
+
+  if(theme == "dark") {
+    btn_dark_DOM.classList.add("active");
+    document.body.classList.add("dark");
+  } else {
+    btn_light_DOM.classList.add("active");
+  }
+}
+fn_theme_init();
+
 function fn_scroll(c_Y) {
   total_height = document.body.clientHeight;
   window_height = document.documentElement.clientHeight;
